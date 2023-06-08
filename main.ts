a57f6672c6d3b32a83ac0e99e60e4e89764631e4
jrflorentine <53338319+jrflorentine@users.noreply.github.com>
2023-06-08 17:58:52 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,13 +6,17 @@ let instruction = ""
 radio.setGroup(1)
 basic.forever(function () {
     if (instruction == "U") {
-        cuteBot.moveTime(cuteBot.Direction.forward, 50, 1)
+        cuteBot.forward()
     } else if (instruction == "D") {
-        cuteBot.moveTime(cuteBot.Direction.backward, 50, 1)
+        cuteBot.backforward()
     } else if (instruction == "L") {
-        cuteBot.moveTime(cuteBot.Direction.left, 50, 1)
+        cuteBot.motors(40, 100)
+        basic.pause(100)
+        cuteBot.forward()
     } else if (instruction == "R") {
-        cuteBot.moveTime(cuteBot.Direction.right, 50, 1)
+        cuteBot.motors(100, 40)
+        basic.pause(100)
+        cuteBot.forward()
     } else if (instruction == "A") {
         cuteBot.motors(100, -100)
     } else if (instruction == "B") {
